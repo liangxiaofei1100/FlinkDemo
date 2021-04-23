6a968248626bbfd6571062da44757400fd89ddb7
Alex Liang <liangxiaofei1100@163.com>
2021-04-23 10:25:12 +0800

    Update JdbcSinkDemo.java

--- src/main/java/com/alex/flink/sink/JdbcSinkDemo.java
+++ src/main/java/com/alex/flink/sink/JdbcSinkDemo.java
@@ -10,7 +10,6 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Time;
 import java.sql.Timestamp;
 
 /**
@@ -32,15 +31,19 @@ public class JdbcSinkDemo {
                 .withDriverName("com.mysql.cj.jdbc.Driver")
                 .withUrl(url).withUsername("root").withPassword("123456").build();
         String sql = "insert into sensor_temp (id, time, temp) values(?,?,?)";
+
+        // JDBC是批量写入，默认的JdbcExecutionOptions中设置是5000条写入一次。如果没有达到批次量，程序结束时也会写入。
         dataStream.addSink(JdbcSink.sink(sql, new JdbcStatementBuilder<SensorReading>() {
             @Override
             public void accept(PreparedStatement preparedStatement, SensorReading sensorReading) throws SQLException {
+                System.out.println("jdbc sink");
                 preparedStatement.setString(1, sensorReading.getId());
                 preparedStatement.setTimestamp(2, new Timestamp(sensorReading.getTimestamp()));
                 preparedStatement.setDouble(3, sensorReading.getTemperature());
             }
         }, jdbcConnectionOptions));
 
+        dataStream.print("sensor");
         environment.execute();
     }
 }
